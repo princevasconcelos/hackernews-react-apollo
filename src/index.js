a982440f4e917323e65fc7e53d6877a039408607
Joaquim <joaquim@princevasconcelos.local>
2018-10-13 10:22:37 -0300

    feat: add <Header />

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
+import { BrowserRouter } from "react-router-dom";
+
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
@@ -19,9 +21,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
