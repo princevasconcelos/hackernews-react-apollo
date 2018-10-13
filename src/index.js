61d30dc5339f54c97692ce73ff154ffca210abac
Joaquim <joaquim@princevasconcelos.local>
2018-10-13 19:53:21 -0300

    feat: add subscriptions

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+
 import { AUTH_TOKEN } from "./constants";
 
 const httpLink = createHttpLink({
@@ -28,10 +32,37 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+//QUERY ou MUTATION -> HttpLink
+//SUBSCRIPTION -> WebSocketLink
+
+const link = split( //(test ? wslink : authLink )
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// });
 
 ReactDOM.render(
   <BrowserRouter>
